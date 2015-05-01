1332233405dc37e404a7e5d6f01144c4c007e6b1
Harney, John F <harneyjf@ornl.gov>
2015-05-01 16:41:54 -0400

    Inserting new url for publishing

--- django/acme_services/publishing/urls.py
+++ django/acme_services/publishing/urls.py
@@ -4,4 +4,5 @@ from publishing import views
 
 urlpatterns = patterns('',
 	url(r'base_facets/(?P<username>\w+)$',views.base_facets,name='base_facets'),
+	url(r'publish_data/(?P<username>\w+)$',views.publish_data,name='publish_data'),
 )
