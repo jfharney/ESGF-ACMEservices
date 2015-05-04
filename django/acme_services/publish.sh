e620208f1617ffeb0da08baf4447312fcef0fb72
root <root@esg.ccs.ornl.gov>
2015-05-04 16:55:15 -0400

    Fix up esgf publish script setup

---
+++ django/acme_services/publish.sh
@@ -0,0 +1,4 @@
+source /etc/esg.env
+export ESGINI=/esg/config/esgcet/esg.ini
+#export ESGINI=/usr/local/esgf-services/ESGF-ACMEservices/django/acme_services/esg.ini 
+esgpublish --map ./ACME.txt --project ACME --publish --service fileservice
